baaa61e8a19971c01f67d084a991fff6ed704eda
Gabriela Sevelkova <gsevelkova5@gmail.com>
2023-10-01 20:32:35 +0100

    Adding Solution

--- primes.py
+++ primes.py
@@ -3,4 +3,20 @@
 
 def primes(number_of_primes):
     list = []
+    number = 2
+    counter = 0
+    if number_of_primes <=0 :
+        raise ValueError
+    while counter < number_of_primes:
+        added = True
+        for y in range(2,number):
+            if number%y == 0 :
+                added = False
+        if added:
+            list.append(number)
+            counter +=1
+        number +=1
+     
     return list
+
+print(primes(24))
